dbcc5abbdf1021d6f8cb144cdb32ee914a9d41ff
Sergey Khomenko <khomenko.mail@gmail.com>
2017-10-14 00:21:02 +0300

    ANgulatJS works, 3 different controllers

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+var vcard = angular.module('vcard', []);
+
+vcard.controller('baseController', function ($scope) {
+	$scope.countryName = 'Ukraine';
+});
+
+vcard.controller('contactController', function ($scope) {
+	// section-contact
+});
+
+vcard.controller('aboutController', function ($scope) {
+	// section-about
+});
